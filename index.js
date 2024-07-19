733f8a5c459c444a0d018463f736dd5651a47ad4
KadenSeaman <kadencs24@outlook.com>
2024-07-18 19:09:11 -0600

    responsivish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const contactMeCloseButton = document.getElementById("contact-me-close-button");
 const contactMeOpenButton = document.getElementById("start-btn");
 const contactMe = document.getElementById("contact-me");
+const firstNameInput = document.getElementById("name");
 
 contactMeCloseButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -10,4 +11,6 @@ contactMeCloseButton.addEventListener("click", (e) => {
 contactMeOpenButton.addEventListener("click",(e) => {
     e.preventDefault();
     contactMe.style.display = "grid";
+    contactMeCloseButton.focus();
+    document.activeElement.blur();
 })
